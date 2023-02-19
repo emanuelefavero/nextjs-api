fdd536761fff9c4c8d04f8bf3db1d5afd039c814
emanuelefavero <emafaux89@gmail.com>
2023-02-19 17:57:23 +0100

    add client data

--- pages/index.tsx
+++ pages/index.tsx
@@ -6,7 +6,57 @@ type Props = {
 }
 
 export default function Home({ data }: Props) {
-  return <>Hello</>
+  return (
+    <>
+      <ul>
+        <li>
+          <span className='title'>OS</span>
+          <span className='colon'>:</span>
+          <span className='value'>{data.operatingSystem}</span>
+        </li>
+        <li>
+          <span className='title'>Architecture</span>
+          <span className='colon'>:</span>
+          <span className='value'>{data.architecture}</span>
+        </li>
+        <li>
+          <span className='title'>Kernel</span>
+          <span className='colon'>:</span>
+          <span className='value'>{data.kernel}</span>
+        </li>
+        <li>
+          <span className='title'>Uptime</span>
+          <span className='colon'>:</span>
+          <span className='value'>{data.uptimeFormatted}</span>
+        </li>
+        <li>
+          <span className='title'>CPU</span>
+          <span className='colon'>:</span>
+          <span className='value'>{data.cpu}</span>
+        </li>
+        <li>
+          <span className='title'>Memory</span>
+          <span className='colon'>:</span>
+          <span className='value'>{data.memory}</span>
+        </li>
+        <li>
+          <span className='title'>Local IP</span>
+          <span className='colon'>:</span>
+          <span className='value'>{data.localIP}</span>
+        </li>
+        <li>
+          <span className='title'>User</span>
+          <span className='colon'>:</span>
+          <span className='value'>{data.osUsername}</span>
+        </li>
+        <li>
+          <span className='title'>Hostname</span>
+          <span className='colon'>:</span>
+          <span className='value'>{data.hostname}</span>
+        </li>
+      </ul>
+    </>
+  )
 }
 
 export const getServerSideProps: GetServerSideProps = async () => {
