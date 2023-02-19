4d5495583caf9f7b62e730d1c58dfa414dc169eb
emanuelefavero <emafaux89@gmail.com>
2023-02-19 22:10:30 +0100

    FIX typo in config/server

--- config/server.ts
+++ config/server.ts
@@ -2,4 +2,4 @@ const dev = process.env.NODE_ENV !== 'production'
 
 export const server = dev
   ? 'http://localhost:3000'
-  : 'https://nextjs-neofetch.vercel.app/'
+  : 'https://nextjs-neofetch.vercel.app'
